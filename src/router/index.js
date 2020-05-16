6d2aef92c768fef363ab539c3d67433c9398fd17
Zhang <18310701744@163.com>
2020-05-16 14:59:19 +0800

    完成了验证码登陆和定位功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,71 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Index = () => import('../views/index')
+const Login = () => import('../views/Login')
+const Home = () => import('../views/Home')
+const Me = () => import('../views/Me')
+const Order = () => import('../views/Order')
+const Address = () => import('../views/Address')
+const City = () => import('../views/City')
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    //name: 'index',
+    component: Index,
+    children: [
+      {
+        path: '',
+        redirect: '/home'
+      },
+      {
+        path: '/home',
+        name: 'home',
+        component: Home
+      },
+      {
+        path: '/order',
+        name: 'order',
+        component: Order
+      },
+      {
+        path: '/me',
+        name: 'me',
+        component: Me
+      },
+      {
+        path: '/address',
+        name: 'address',
+        component: Address
+      },
+      {
+        path: '/city',
+        name: 'city',
+        component: City
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/login', name: 'login', component: Login }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.ele_login ? true : false
+  if (to.path == '/login') {
+    next()
+  } else {
+    // 是否在登陆状态下
+    isLogin ? next() : next('/login')
+  }
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="login">
+    <div class="logo">
+      <img src="../assets/logo.jpg" alt=" my login image" />
+    </div>
+    <!-- 手机号 -->
+    <input-group
+      type="number"
+      v-model="phone"
+      placeholder="手机号"
+      :btnTitle="btnTitle"
+      :disabled="disabled"
+      :error="errors.phone"
+      @btnClick="getVerifyCode"
+    ></input-group>
+    <!-- 验证码 -->
+    <input-group type="number" v-model="verifyCode" placeholder="验证码" :error="errors.code"></input-group>
+    <!-- 用户协议服务 -->
+    <div class="login_des">
+      <p>
+        新用户登录即自动注册，表示已同意
+        <span>《用户服务协议》</span>
+      </p>
+    </div>
+    <!-- 登录按钮 -->
+    <div class="login_btn">
+      <button :disabled="isClick" @click="handleLogin">登陆</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputGroup from '../components/inputGroup'
+export default {
+  name: 'Login',
+  components: {
+    InputGroup
+  },
+  data() {
+    return {
+      phone: '', // 手机号：18810306704 验证码：850544
+      verifyCode: '',
+      errors: {},
+      btnTitle: '获取验证码',
+      disabled: false
+    }
+  },
+  computed: {
+    isClick() {
+      if(!this.phone || !this.verifyCode) return true
+      else false
+    }
+  },
+  methods: {
+    // 登陆
+    handleLogin() {
+      // 取消错误提醒
+      this.errors = {}
+      // 发送请求
+      this.$axios.post('/api/posts/sms_back', {
+        phone: this.phone,
+        code: this.verifyCode
+      })
+      .then(res => {
+        console.log(res);
+        // 检验成功 设置登陆状态并且跳转到home页面
+        localStorage.setItem('ele_login', true)
+        this.$router.push('/')         
+      })
+      .catch(err => {
+        //返回错误信息
+        this.errors = {
+          code: err.response.data.msg
+          //code: err.response
+        }
+      })
+    },
+    getVerifyCode() {
+      if (this.validatePhone()) {
+        this.validateBtn()
+        // 发送网络请求
+        this.$axios
+          .post('/api/posts/sms_send', {
+            tpl_id: '215179',
+            key: '62b4ba3de6eca73f7bd57b888d748ef0',
+            phone: this.phone
+          })
+          .then(res => {
+            console.log(res)
+          })
+      }
+    },
+    // 获取验证码
+    validateBtn() {
+      let time = 60
+      let timer = setInterval(() => {
+        if (time == 0) {
+          clearInterval(timer)
+          this.btnTitle = '获取验证码'
+          this.disabled = false
+        } else {
+          // 倒计时
+          this.btnTitle = time + '秒后重试'
+          this.disabled = true
+          time--
+        }
+      }, 1000)
+    },
+    // 验证手机号码
+    validatePhone() {
+      if (!this.phone) {
+        this.errors = {
+          phone: '手机号码不能为空'
+        }
+        return false
+      } else if (!/^1[345678]\d{9}$/.test(this.phone)) {
+        this.errors = {
+          phone: '请填写正确的手机号码'
+        }
+        return false
+      } else {
+        this.errors = {}
+        return true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+  box-sizing: border-box;
+  background: #fff;
+}
+.logo {
+  text-align: center;
+}
+.logo img {
+  width: 150px;
+}
+.text_group,
+.login_des,
+.login_btn {
+  margin-top: 20px;
+}
+.login_des {
+  color: #aaa;
+  line-height: 22px;
+}
+.login_des span {
+  color: #4d90fe;
+}
+.login_btn button {
+  width: 100%;
+  height: 40px;
+  background-color: #48ca38;
+  border-radius: 4px;
+  color: white;
+  font-size: 14px;
+  border: none;
+  outline: none;
+}
+.login_btn button[disabled] {
+  background-color: #8bda81;
+}
+</style>
